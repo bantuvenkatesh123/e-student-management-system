6a501117820232f605d50371e8419e1e09b02cd5
unknown <bantuvenkatesh123@gmail.com>
2022-05-17 18:59:57 +0530

    first commit

---
+++ redirect.js
@@ -0,0 +1,16 @@
+function redirect(){
+    alert("Redirecting to login page");
+    window.location.href='https://intranet.rguktn.ac.in/SMS/';
+}
+function redirect1(){
+    alert("Redirecting to CDPC login page");
+    window.location.href='https://cdpc.rguktn.ac.in';
+}
+function redirect2(){
+    alert("Redirecting to library web page");
+    window.location.href='https://rguktn.ac.in/library/';
+}
+function redirect3(){
+    alert("Redirecting to examination results page");
+    window.location.href='https://examcell.rguktn.ac.in/';
+}
